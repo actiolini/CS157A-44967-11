4d83fb19601b1333bc881b19ebd242a7fb60d09e
ThuanChau22 <thuan.chau@sjsu.edu>
2020-12-18 17:21:40 -0800

    Add main class

---
+++ src/main/java/moviebuddy/Try.java
@@ -0,0 +1,11 @@
+package moviebuddy;
+
+// import moviebuddy.util.Passwords;
+// import moviebuddy.util.Validation;
+// import moviebuddy.dao.UserDAO;
+
+public class Try {
+    public static void main(String[] args) {
+        System.out.println("Try:");
+    }
+}
