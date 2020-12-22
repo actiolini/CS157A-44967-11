bc1145da11936f62f70b0023c3aa59290b9808bb
ThuanChau22 <thuan.chau@sjsu.edu>
2020-12-22 06:06:27 -0800

    Add class for session variable names

---
+++ src/main/java/moviebuddy/util/S.java
@@ -0,0 +1,147 @@
+package moviebuddy.util;
+
+public class S {
+    // JSP pages
+    public static final String HOME_PAGE = "home.jsp";
+    public static final String NAV_BAR_PAGE = "navbar.jsp";
+    public static final String FOOTER_PAGE = "footer.jsp";
+    public static final String SIGN_UP_PAGE = "signup.jsp";
+    public static final String SIGN_IN_PAGE = "signin.jsp";
+    public static final String MANAGE_THEATRE_PAGE = "managetheatre.jsp";
+    public static final String THEATRE_CREATE_PAGE = "theatrecreate.jsp";
+    public static final String THEATRE_EDIT_PAGE = "theatreedit.jsp";
+    public static final String MANAGE_TICKET_PRICE_PAGE = "manageticketprice.jsp";
+    public static final String MANAGE_ROOM_PAGE = "manageroom.jsp";
+    public static final String ROOM_CREATE_PAGE = "roomcreate.jsp";
+    public static final String ROOM_EDIT_PAGE = "roomedit.jsp";
+    public static final String MANAGE_MOVIE_PAGE = "managemovie.jsp";
+    public static final String MOVIE_CREATE_PAGE = "moviecreate.jsp";
+    public static final String MOVIE_EDIT_PAGE = "movieedit.jsp";
+    public static final String MANAGE_SCHEDULE_PAGE = "manageschedule.jsp";
+    public static final String MANAGE_STAFF_PAGE = "managestaff.jsp";
+    public static final String STAFF_SIGN_IN_PAGE = "staffsignin.jsp";
+    public static final String STAFF_SIGN_UP_PAGE = "staffsignup.jsp";
+    public static final String ERROR_PAGE = "error.jsp";
+    public static final String CHECK_OUT_PAGE = "checkout.jsp";
+    public static final String PROFILE_PAGE = "customerPortal.jsp";
+
+    // session
+    public static final String SESSION_ID = "sessionId";
+    public static final String CURRENT_SESSION = "currentSession";
+
+    // role
+    public static final String ADMIN = "admin";
+    public static final String MANAGER = "manager";
+    public static final String FACULTY = "faculty";
+
+    // user
+    public static final String ACCOUNT_ID = "accountId";
+    public static final String USERNAME = "userName";
+    public static final String EMAIL = "email";
+    public static final String ZIPCODE = "zipcode";
+    public static final String BUDDY_POINTS = "buddyPoints";
+    public static final String AUTO_RENEW = "autoRenew";
+    public static final String END_DATE = "endDate";
+    public static final String STAFF_ID = "staffId";
+    public static final String ROLE = "role";
+    public static final String EMPLOY_THEATRE_ID = "employTheatreId";
+    public static final String STAFF_ID_INPUT = "signinStaffId";
+
+    // signup
+    public static final String SIGN_UP_USERNAME = "signupUserName";
+    public static final String SIGN_UP_EMAIL = "signupEmail";
+
+    // signup staff
+    public static final String SIGN_UP_STAFF_ROLE = "signupStaffRole";
+    public static final String SIGN_UP_STAFF_LOCATION = "signupStaffTheatreLocation";
+    public static final String SIGN_UP_STAFF_USERNAME = "signupStaffUserName";
+    public static final String SIGN_UP_STAFF_EMAIL = "signupStaffEmail";
+
+    // signin
+    public static final String SIGN_IN_EMAIL = "signInEmail";
+
+    // signin staff
+    public static final String SIGN_IN_STAFF_ID = "signinStaffId";
+
+    //admin
+    public static final String SELECTED_THEATRE_ID = "selectTheatreId";
+    public static final String SELECTED_THEATRE_NAME = "selectTheatreName";
+
+    // manage theatre
+    public static final String THEATRE_LIST = "theatreList";
+
+    // theatre create
+    public static final String THEATRE_CREATE_NAME = "theatreNameCreate";
+    public static final String THEATRE_CREATE_ADDRESS = "theatreAddressCreate";
+    public static final String THEATRE_CREATE_CITY = "theatreCityCreate";
+    public static final String THEATRE_CREATE_STATE = "theatreStateCreate";
+    public static final String THEATRE_CREATE_COUNTRY = "theatreCountryCreate";
+    public static final String THEATRE_CREATE_ZIP = "theatreZipCreate";
+
+    // theatre edit
+    public static final String THEATRE_EDIT_ID = "theatreIdEdit";
+    public static final String THEATRE_EDIT_NAME = "theatreNameEdit";
+    public static final String THEATRE_EDIT_ADDRESS = "theatreAddressEdit";
+    public static final String THEATRE_EDIT_CITY = "theatreCityEdit";
+    public static final String THEATRE_EDIT_STATE = "theatreStateEdit";
+    public static final String THEATRE_EDIT_COUNTRY = "theatreCountryEdit";
+    public static final String THEATRE_EDIT_ZIP = "theatreZipEdit";
+
+    // manage ticket price
+    public static final String TICKET_THEATRE_ID = "ticketTheatreId";
+    public static final String TICKET_THEATRE_NAME = "ticketTheatreName";
+    public static final String TICKET_PRICE_LIST = "ticketPriceList";
+    public static final String TICKET_START_TIME_CREATE = "ticketStartTimeCreate";
+    public static final String TICKET_PRICE_CREATE = "ticketPriceCreate";
+
+    // manage staff
+    public static final String ADMIN_LIST = "adminList";
+    public static final String STAFF_LIST = "staffList";
+
+    // manage room
+    public static final String ROOM_THEATRE_ID = "roomTheatreId";
+    public static final String ROOM_THEATRE_NAME = "roomTheatreName";
+    public static final String ROOM_LIST = "roomList";
+
+    // room create
+    public static final String ROOM_CREATE_NUMBER = "roomNumberCreate";
+    public static final String ROOM_CREATE_SECTIONS = "roomSectionsCreate";
+    public static final String ROOM_CREATE_SEATS = "roomSeatsCreate";
+
+    // room edit
+    public static final String ROOM_EDIT_ID = "roomIdEdit";
+    public static final String ROOM_EDIT_NUMBER = "roomNumberEdit";
+    public static final String ROOM_EDIT_SECTIONS = "roomSectionsEdit";
+    public static final String ROOM_EDIT_SEATS = "roomSeatsEdit";
+
+    // manage movie
+    public static final String MOVIE_LIST = "movieList";
+
+    // movie create
+    public static final String MOVIE_CREATE_TITLE = "movieTitleCreate";
+    public static final String MOVIE_CREATE_RELEASE_DATE = "movieReleaseDateCreate";
+    public static final String MOVIE_CREATE_DURATION = "movieDurationCreate";
+    public static final String MOVIE_CREATE_TRAILER = "movieTrailerCreate";
+    public static final String MOVIE_CREATE_DESCRIPTION = "movieDescriptionCreate";
+
+    // move edit
+    public static final String MOVIE_EDIT_ID = "movieIdEdit";
+    public static final String MOVIE_EDIT_TITLE = "movieTitleEdit";
+    public static final String MOVIE_EDIT_RELEASE_DATE = "movieReleaseDateEdit";
+    public static final String MOVIE_EDIT_DURATION = "movieDurationEdit";
+    public static final String MOVIE_EDIT_TRAILER = "movieTrailerEdit";
+    public static final String MOVIE_EDIT_DESCRIPTION = "movieDescriptionEdit";
+
+    // manage schedule
+    public static final String SCHEDULE_THEATRE_ID = "scheduleTheatreId";
+    public static final String SCHEDULE_THEATRE_NAME = "scheduleTheatreName";
+    public static final String SCHEDULE_MOVIE_ID = "scheduleMovieId";
+    public static final String SCHEDULE_MOVIE_TITLE = "scheduleMovieTitle";
+    public static final String SCHEDULE_LIST = "scheduleList";
+    public static final String SCHEDULE_SHOW_DATE_CREATE = "scheduleShowDateCreate";
+    public static final String SCHEDULE_START_TIME_CREATE = "scheduleStartTimeCreate";
+    public static final String SCHEDULE_ROOM_NUMBER_CREATE = "scheduleRoomNumberCreate";
+
+    // error
+    public static final String ERROR_MESSAGE = "errorMessage";
+}
